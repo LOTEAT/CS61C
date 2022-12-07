d0f31e39d11119327b00d2e4a9bd141244bcc74a
LOTEAT <zenglezhu@gmail.com>
2022-12-07 23:09:21 +0800

    relu

--- src/relu.s
+++ src/relu.s
@@ -14,11 +14,20 @@
 # ==============================================================================
 relu:
     # Prologue
-
+    # store some values
+    addi sp, sp, -12
+    sw t0, 0(sp)
+    sw t1, 4(sp)
+    # load value from array
+    sw s0, 8(sp)
+    # array address
+    mv t0, a0
+    # array index, start from 0
+    mv t1, 0
 
 loop_start:
-    
-
+    bne t1, loop_end
+    lw 
 
 
 
