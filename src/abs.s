1d52b2c1e82217ede7c53e8eec1faf9ccb0d6ca3
LOTEAT <zenglezhu@gmail.com>
2022-12-07 22:30:51 +0800

    redo

--- src/abs.s
+++ src/abs.s
@@ -9,10 +9,11 @@
 #	a0 (int) the absolute value of the input
 # =================================================================
 abs:
+    # Prologue
+    
+    # return 0
+    mv a0, zero
 
-    # branch if positive
-    bge a0, zero, done
-    # invert a if negative
-    sub a0, zero, a0
-done:
+    # Epilogue
+
     ret
